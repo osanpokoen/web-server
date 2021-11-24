546ca629f5b8da6199fc2e59d1f76a50686ea12e
kakitamama <84656504+kakitamama@users.noreply.github.com>
2021-11-24 18:54:28 +0900

    Add CircleCI config

--- main.tf
+++ main.tf
@@ -26,3 +26,43 @@ resource "aws_ecr_lifecycle_policy" "default" {
     ]
   })
 }
+
+# IAM User used by CircleCI
+resource "aws_iam_user" "default" {
+  name = "circleci_web-server"
+}
+
+resource "aws_iam_policy" "default" {
+  name = "circleci_web-server"
+
+  policy = <<-POLICY
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ecr:GetAuthorizationToken"
+                ],
+                "Resource": "*"
+            },
+            {
+                "Effect": "Allow",
+                "Action": [
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:CompleteLayerUpload",
+                    "ecr:InitiateLayerUpload",
+                    "ecr:PutImage",
+                    "ecr:UploadLayerPart"
+                ],
+                "Resource": "${aws_ecr_repository.default.arn}"
+            }
+        ]
+    }
+    POLICY
+}
+
+resource "aws_iam_user_policy_attachment" "default" {
+  user       = aws_iam_user.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
